879321bd030fd6fa2293a0bcb0ab51e19ab2f93c
Dr-SlipStream <rohittandel004@gmail.com>
2026-04-05 17:58:24 +0530

    updating for automation

--- api/energy.py
+++
@@ -1,9 +0,0 @@
-import librosa
-import numpy as np
-
-def extract_energy(audio):
-    """
-    Extract RMS energy from audio signal
-    """
-    rms = librosa.feature.rms(y=audio)
-    return np.mean(rms)
--- api/mfcc.py
+++
@@ -1,9 +0,0 @@
-import librosa
-import numpy as np
-
-def extract_mfcc(audio, sr, n_mfcc=13):
-    """
-    Extract MFCC features from audio signal
-    """
-    mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=n_mfcc)
-    return np.mean(mfcc, axis=1)
